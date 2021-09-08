04d89b653853325572c3d477816a6c04a8c8057e
Kousic Lattala <kousiclattala112@gmail.com>
2021-09-08 15:59:52 +0530

    Made changes to backend

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+import { createApi } from "unsplash-js";
 
+const app = express();
+// middleware
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
 
+app.use(express.json());
+
+// Api initialization
+const unsplash = createApi({
+  accessKey: "kGUu_gqO1GtfvG_w7wKsFmDjYdVVKk-VgifiuHJqdis",
+  fetch: fetch,
+});
+
+// routes
+app.post("/search", (req, res) => {
+  const name = req.body.name;
+
+  unsplash.search
+    .getPhotos({
+      query: name,
+      page: 1,
+      perPage: 10,
+    })
+    .then((result) => {
+      if (result.errors) {
+        return res.status(400).json({
+          err: result.errors[0],
+        });
+      } else {
+        res.status(200).json({
+          photos: result.response.results,
+        });
+      }
+    });
+});
+
+// server configuration
 const PORT = 5000;
 
 app.listen(PORT, () => {
