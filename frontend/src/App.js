bf64403804a35555ec6a21610631605435a8c325
Kousic Lattala <kousiclattala112@gmail.com>
2021-09-09 16:26:45 +0530

    Added DragnDrop Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,26 @@ import React from "react";
 import BottomPanel from "./components/BottomPanel";
 import CarouselPanel from "./components/CarouselPanel";
 import LeftPanel from "./components/LeftPanel";
-
+import { useSelector, useDispatch } from "react-redux";
 import { DragDropContext } from "react-beautiful-dnd";
+import { getPhotos } from "./redux/photoSlice";
 
 const App = () => {
+  const photos = useSelector((state) => state.photo.photos);
+  const dispatch = useDispatch();
+
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    const list1 = [];
+    Object.keys(photos).map(([k]) => list1.push(photos[k]));
+    const removed = list1.splice(source.index, 1);
+    list1.splice(destination.index, 0, removed);
+    dispatch(getPhotos(list1));
+  };
+
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div className="row">
         <h1 className="text-center text-white">Carousel</h1>
         <div className="col col-2">
--- frontend/src/components/BottomPanel.js
+++ frontend/src/components/BottomPanel.js
@@ -1,26 +1,44 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 
 const BottomPanel = () => {
   const photos = useSelector((state) => state.photo.photos);
 
   return (
     <div className="mt-3 flex-row bg-dark p-2">
-      <div className="row">
-        {Object.keys(photos).map(([k]) => (
-          <div className="col">
-            <div className="card" key={photos[k].id}>
-              <img
-                className="img-thumbnail d-block"
-                src={photos[k].urls?.thumb}
-                alt={photos[k].id}
-                width={200}
-                height={200}
-              />
-            </div>
+      <Droppable droppableId="dropId">
+        {(provided) => (
+          <div
+            className="row"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {Object.keys(photos).map(([k]) => (
+              <Draggable key={photos[k].id} index={k}>
+                {(provided) => (
+                  <div
+                    className="col"
+                    key={photos[k].id}
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <img
+                      className="img-thumbnail d-block"
+                      src={photos[k].urls?.thumb}
+                      alt={photos[k].id}
+                      width={200}
+                      height={200}
+                    />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
           </div>
-        ))}
-      </div>
+        )}
+      </Droppable>
     </div>
   );
 };
